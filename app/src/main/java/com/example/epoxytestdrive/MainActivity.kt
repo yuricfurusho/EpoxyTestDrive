e93b666a4a679b76b4edff7826be8cc1f3538069
yurifurusho <yuri>
2019-06-12 16:23:42 -0400

    1.1 add epoxy recycler
    add special epoxy recycler

--- app/src/main/java/com/example/epoxytestdrive/MainActivity.kt
+++ app/src/main/java/com/example/epoxytestdrive/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.epoxytestdrive
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.epoxy.EpoxyRecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: EpoxyRecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.epoxy_recycler_view)
+
     }
 }
